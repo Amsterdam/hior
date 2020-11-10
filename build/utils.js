60349a44fc92000443704c5d69384acd3098ddb6
Jasper Swart <jasper.g.swart@gmail.com>
2020-11-10 16:37:00 +0100

    disabled ExtractTextPlugin

--- build/utils.js
+++ build/utils.js
@@ -49,10 +49,11 @@ exports.cssLoaders = function(options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: "vue-style-loader"
-      });
+      // DISABLED because of webpack 4+
+      // return ExtractTextPlugin.extract({
+      //   use: loaders,
+      //   fallback: "vue-style-loader"
+      // });
     } else {
       return ["vue-style-loader"].concat(loaders);
     }
